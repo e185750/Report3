89c508de53df839b73321c7aab6fbb4478eb979c
Syouya Takahashi <e185750@ie.u-ryukyu.ac.jp>
2018-11-12 23:29:30 +0900

    1st commit

--- src/main/jp/ac/uryukyu/ie/e185750/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e185750/Enemy.java
@@ -49,6 +49,10 @@ public class Enemy {
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
+        if (dead == false){
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+        }
     }
 
     /**
